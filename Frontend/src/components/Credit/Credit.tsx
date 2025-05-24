b6686d1f39773a9b8696febb33ddd984f85027ed
JosipSankovic <sankovic52@gmail.com>
2025-05-24 15:29:04 +0200

    dodan sidebar

---
+++ Frontend/src/components/Credit/Credit.tsx
@@ -0,0 +1,11 @@
+
+
+
+export default function Credit(){
+
+    return(
+        <div>
+            <h1>Credit</h1>
+        </div>
+    )
+}
---
+++ Frontend/src/components/FinanceSplit/FinanceSplit.jsx
@@ -0,0 +1,7 @@
+
+export default function FinanceSplit(){
+
+    return(<div>
+        <h1>Finance spliter</h1>
+    </div>)
+}
